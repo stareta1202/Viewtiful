ed9cc87c75d7b028114ab78370f120d2eaa9073c
stareta1202 <stareta1202@gmail.com>
2021-10-11 18:50:16 +0900

    Add Declarative UIKit

--- UIStackView+updateArrangedSubviews.swift
+++ UIStackView+updateArrangedSubviews.swift
@@ -5,4 +5,14 @@
 //  Created by 이용준 on 2021/10/11.
 //
 
-import Foundation
+import UIKit
+
+extension UIStackView {
+    func updateArrangedSubviews<Model: Equatable, View: UIView> (
+        _ newModels: [Model],
+        _ oldModels: [Model],
+        _ viewFactory: (Model) -> View?,
+        _ viewConfig: (View, Model) -> Void = {_, _ in }
+    ) {
+    }
+}
